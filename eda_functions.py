d898b6f1ff7c55bdac8113b2c4d6e8c3368b780b
johna ramal <johnailia51@gmail.com>
2024-07-26 11:24:28 +0500

    made project more maintainable

---
+++ eda_functions.py
@@ -0,0 +1,12 @@
+import ast
+import pandas as pd
+
+def convert_job_skills(x):
+    '''Convert string to list'''
+    try:
+        if pd.notna(x):
+            return ast.literal_eval(x)
+        else:
+            return x
+    except (ValueError, SyntaxError):
+        return x
